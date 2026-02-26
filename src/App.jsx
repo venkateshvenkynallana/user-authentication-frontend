98744b493e8a891194ec92b13a3a61acce14354c
Venkatesh Nallana <venkateshnallana2003@gmail.com>
2026-02-26 18:41:43 +0530

    Fix import path for Dashboard componentsomething

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
+import Dashboard from './pages/DashBoard'
 import './styles/App.css'
 import Register from './pages/Register'
 import { ToastContainer } from 'react-toastify'
